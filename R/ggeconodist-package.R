d8d124091511725c1f901ca06ffae1ecaf778920
boB Rudis <bob@rud.is>
2019-07-14 09:55:20 -0400

    R package repo initialization complete

---
+++ R/ggeconodist-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/ggeconodist>
+#' - BugReports: <https://gitlab.com/hrbrmstr/ggeconodist/issues>
+#' 
+#' @md
+#' @name ggeconodist
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+"_PACKAGE"
