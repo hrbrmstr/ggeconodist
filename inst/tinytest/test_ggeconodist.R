6709f5e07272b3ab9bcc751f14482e3688c5b280
boB Rudis <bob@rud.is>
2019-07-17 06:24:32 -0400

    comma gone

--- inst/tinytest/test_ggeconodist.R
+++ inst/tinytest/test_ggeconodist.R
@@ -1,4 +1,24 @@
+library(ggeconodist)
 
-# Placeholder with simple test
-expect_equal(1 + 1, 3)
+ggplot(mammogram_costs, aes(x = city)) +
+  geom_econodist(
+    aes(ymin = tenth, median = median, ymax = ninetieth),
+    stat = "identity",
+  ) -> gg
 
+gb <- ggplotGrob(gg)
+
+expect_true(inherits(gb, "gtable"))
+expect_true(identical(dim(gb$layout), c(18L, 7L)))
+
+add_econodist_legend(
+  gg,
+  econodist_legend_grob(
+    tenth_col = "#b07aa1",
+    ninetieth_col = "#591a4f",
+  ),
+  below = "subtitle",
+  just = "right"
+) -> gt
+
+expect_true("econodist-legend" %in% gt$layout$name)
